ef6a52fc47350fff8fbf00da27bdee93a4547343
Mark Gargan <mark@marks-mbp.home>
2016-03-07 22:39:53 +0000

    First commit

---
+++ src/main/java/com/tekenable/controller/AppController.java
@@ -0,0 +1,15 @@
+package com.tekenable.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/trialdirect")
+public class AppController {
+
+  @RequestMapping(method = RequestMethod.GET)
+  public String viewApplication() {
+    return "index";
+  }
+}
