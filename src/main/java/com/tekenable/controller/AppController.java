413c69d6b444c935268b2545a59f772d602a5912
Mark Gargan <mark@marks-mbp.home>
2016-03-07 22:46:06 +0000

    REST Change

--- src/main/java/com/tekenable/controller/AppController.java
+++ src/main/java/com/tekenable/controller/AppController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/trialdirect")
+@RequestMapping("/")
 public class AppController {
 
   @RequestMapping(method = RequestMethod.GET)
