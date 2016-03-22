ae092852ddd3baf1a36e74b43e8c970402dba6ec
Mark Gargan <mark.gargan@gmail.com>
2016-03-22 10:53:21 +0000

    Precursor for spring security

---
+++ src/main/java/com/tekenable/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package com.tekenable.config;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * No customizations of {@link AbstractSecurityWebApplicationInitializer} are necessary.
+ *
+ * @author Rob Winch
+ */
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
