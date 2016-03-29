b6aee6d2c42f556cd1a3a8df6a5da5c42b3f045a
Mark Gargan <mark.gargan@gmail.com>
2016-03-29 13:42:47 +0100

    Three form functional completion

--- src/main/java/com/tekenable/model/Trial.java
+++ src/main/java/com/tekenable/model/Trial.java
@@ -1,11 +1,46 @@
 package com.tekenable.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.List;
 import java.util.Set;
+
+@NamedQueries({
+
+
+        @NamedQuery(name = "Trial.getAllTrialsForUserId",
+                query = "select t from Trial t where id=:userId"),
+
+//        @NamedQuery(name = "Trial.getAvailableTrials",
+//                query = "select t2 from Trial t2 where t2.id not in ( " +
+//                        "select t from Trial t " +
+//                        "inner join TherapeuticArea ta " +
+//                        "inner join User u on t.therapeutic_area_id = u.therapeutic_area_id " +
+//                        "inner join UserSelectorQuestionnaireEntry usqe " +
+//                        "inner join TrialSelectorQuestionnaireEntry tsqe " +
+//                        "  on usqe.question_id=tsqe.question_id " +
+//                        "    and usqe.answer_id=tsqe.answer_id " +
+//                        "      where u.id=:userId and usqe.therapeuticArea.id=:therapeuticArea)")
+})
+
+@NamedNativeQueries({
+        @NamedNativeQuery(name="Trial.getAvailableTrialIds",
+                query="select matching_trial.id from Trial matching_trial  where matching_trial.id not in ( " +
+                        "select distinct (trial_id) from Trial t " +
+                        "  inner join User u on t.therapeutic_area_id = u.therapeutic_area_id " +
+                        "  inner join UserSelectorQuestionnaireEntry usqe " +
+                        "  inner join TrialSelectorQuestionnaireEntry tsqe " +
+                        "    on usqe.question_id=tsqe.question_id " +
+                        "       and usqe.answer_id=tsqe.answer_id " +
+                        "where user_id=:userId) " +
+                        "and matching_trial.therapeutic_area_id = :usersTherapeuticAreaId")
+})
+
 
 @Entity
 @EntityListeners(TrialListener.class)
-public class Trial extends BaseEntity{
+public class Trial extends BaseEntity {
 
     private String title;
 
@@ -13,9 +48,10 @@ public class Trial extends BaseEntity{
 
     private TherapeuticArea therapeuticArea;
 
-    public Trial() {}
+    public Trial() {
+    }
 
-    public Trial( String title, TherapeuticArea therapeuticArea) {
+    public Trial(String title, TherapeuticArea therapeuticArea) {
         this.title = title;
         this.therapeuticArea = therapeuticArea;
     }
@@ -28,6 +64,7 @@ public class Trial extends BaseEntity{
         this.title = title;
     }
 
+    @JsonIgnore
     @OneToMany(mappedBy = "trial", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     public Set<TrialSelectorQuestionnaireEntry> getTrialselectorquestionnaireentries() {
         return trialselectorquestionnaireentries;
@@ -37,6 +74,8 @@ public class Trial extends BaseEntity{
         this.trialselectorquestionnaireentries = trialselectorquestionnaireentries;
     }
 
+
+//    @JsonIgnore
     @ManyToOne
     @JoinColumn(nullable = true, name = "therapeutic_area_id")
     public TherapeuticArea getTherapeuticArea() {
---
+++ src/main/webapp/app/trials/services/TrialService.js
@@ -0,0 +1,33 @@
+angular.module('trialdirect').service('TrialService', ['$http', function($http){
+
+    var AVAILABLE_TRIALS_COUNT_URL = './availabletrialids';
+
+    var AVAILABLE_TRIALS_URL = './availabletrial';
+
+    function getAvailableTrialsCount(userId, therapeuticAreaId, callback) {
+        return $http.get(AVAILABLE_TRIALS_COUNT_URL + '/' + userId + '/therapeuticarea/' + therapeuticAreaId)
+            .then (function(trials){
+
+            if (trials == [])
+                trials.data = [];
+
+            callback && callback(trials.data);
+        })
+    }
+
+    function getAvailableTrials(userId, therapeuticAreaId) {
+        $http.get(AVAILABLE_TRIALS_URL + '/' + userId).then (function(trials){
+
+            return _.map(trials, function (trial) {
+
+                return new TrialResourceService(trial);
+            });
+        })
+    }
+
+    return {
+        getAvailableTrials:getAvailableTrials,
+        getAvailableTrialsCount:getAvailableTrialsCount
+    }
+
+}]);
