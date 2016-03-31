41b8259ec0611049e6e96b0bf845396315e85056
Mark Gargan <mark.gargan@gmail.com>
2016-03-31 14:55:27 +0100

    TrialSelector is now saved at the end of question selections

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -19,98 +19,10 @@ angular.module('trialdirect', [
 
                 $rootScope.$on("$stateChangeError", console.log.bind(console));
 
-                //$rootScope.$on('$stateChangeSuccess',
-                //    function (event, toState, toParams, fromState, fromParams) {
-                //        console.log('Changing state from :-');
-                //        console.log(fromState);
-                //        console.log('to state :-');
-                //        console.log(toState);
-                //
-                //    })
             }
         ]
     ).config(['$stateProvider', '$urlRouterProvider',
     function ($stateProvider, $urlRouterProvider) {
-
-        var trial = {
-            templateUrl: 'views/templates/trial.view.htm',
-            controller: 'TrialController',
-            resolve: {
-                trials: function (TrialResourceService) {
-                    return TrialResourceService.load();
-                }
-            }
-        };
-
-
-        var therapeuticAreaState = {
-            templateUrl: 'app/therapeuticareas/views/therapeutic.area.questionnaire.view.htm',
-            controller: 'TherapeuticAreaEditController',
-
-            // We use the 'resolve' below to prime the page with the therapeutic area
-            // so that they're available before the page loads preventing the flickering
-            // as the bound questions hit the scope
-            resolve: {
-
-                therapeuticAreas: function (TherapeuticAreaResourceService) {
-
-                    // Initialise the query api and return all available Therapeutic Areas
-                    return TherapeuticAreaResourceService.load();
-                }
-            }
-        };
-
-        var questionnaireState = {
-            templateUrl: 'views/templates/questionnaire.view.htm',
-            controller: 'QuestionnaireController',
-
-            // We use the 'resolve' below to prime the page with the questions
-            // so that they're available before the page loads preventing the flickering
-            // as the bound questions hit the scope
-            resolve: {
-
-                questionnaireEntries: function (QuestionnaireEntryResourceService, Answer) {
-                    //Initialise the answer api
-                    Answer.initialize();
-
-                    // Initialise the query api and return all available questions
-                    return QuestionnaireEntryResourceService.load();
-                }
-            }
-        };
-
-        $stateProvider
-            .state('home', {
-                url: "/",
-                templateUrl: 'views/templates/home.view.htm',
-                views: {
-                    "viewA": therapeuticAreaState
-                    ,
-                    "viewB": questionnaireState
-                }
-            })
-            .state('trial', {
-                url: '/trial',
-                views: {
-                    "viewA": trial
-                }
-            })
-            .state('therapeuticarea', {
-                url: '/therapeuticarea',
-                views: {
-                    "viewA": therapeuticAreaState
-                }
-            })
-            .state('item', {
-                url: '/item',
-                templateUrl: 'views/templates/item.view.htm',
-                controller: 'AppController'
-            }).state('questionnaire', {
-            url: '/questionnaire',
-            views: {
-                "viewA": questionnaireState
-            }
-        });
 
         $urlRouterProvider.otherwise('/therapeuticareas');
     }]).filter('reverse', function () {
