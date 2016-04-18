aa502d7394eeeb8f5055b45e5e03e01966e8afab
smoczyna <jaroslaw.smorczewski@tekenable.com>
2016-04-18 15:29:42 +0100

    dev maven profile added (with full run) openshift profile skips  intergration tests based on tomcat plugin

--- src/main/java/com/tekenable/config/AuditConfigurator.java
+++ src/main/java/com/tekenable/config/AuditConfigurator.java
@@ -2,48 +2,13 @@ package com.tekenable.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AuditConfigurator {
-
-    private class TrialDirectTableDef {
-        private String columnName;
-        private String columnDef;
-
-        public String getColumnName() {
-            return columnName;
-        }
-
-        public void setColumnName(String columnName) {
-            this.columnName = columnName;
-        }
-
-        public String getColumnDef() {
-            return columnDef;
-        }
-
-        public void setColumnDef(String columnDef) {
-            this.columnDef = columnDef;
-        }
-    }
-
-    private class TrialDirectColDefMapper implements RowMapper {
-
-        @Override
-        public TrialDirectTableDef mapRow(ResultSet resultSet, int i) throws SQLException {
-            TrialDirectTableDef def = new TrialDirectTableDef();
-            def.setColumnName(resultSet.getString("column_name"));
-            def.setColumnDef(resultSet.getString("column_type"));
-            return def;
-        }
-    }
 
     @Autowired
     protected DataSource dataSource;
@@ -53,14 +18,14 @@ public class AuditConfigurator {
 
         JdbcTemplate jdbc = new JdbcTemplate(dataSource);
 
-        /*String auditTablesSQL = "select table_name from information_schema.tables \n" +
+        String auditTablesSQL = "select table_name from information_schema.tables \n" +
                                 "where table_schema = 'trialdirect' \n" +
                                 "and table_name like '%_audit'";
         List<String> auditTables = jdbc.queryForList(auditTablesSQL, String.class);
         for (String table : auditTables) {
             int result = jdbc.update("drop table if exists ".concat(table));
             System.out.println(String.format("drop table %1$s executed with result: %2$d", table, result));
-        }*/
+        }
 
         String tablesSQL = "select table_name from information_schema.tables \n" +
                            "where table_schema = 'trialdirect'\n" +
@@ -73,22 +38,16 @@ public class AuditConfigurator {
         operations.put("_AD", "DELETE");
 
         for (String table : tables) {
+            String tabColumnsDefSQL = "select concat(column_name, ' ', column_type) from information_schema.columns\n" +
+                                      "where table_schema = 'trialdirect' and table_name = '#TAB#' order by ordinal_position";
+            tabColumnsDefSQL = tabColumnsDefSQL.replace("#TAB#", table);
+            List<String> columnsDef = jdbc.queryForList(tabColumnsDefSQL, String.class);
             String auditTableName = table.concat("_audit");
 
-            String auditTabColumnsSQL = "select column_name, concat(column_name, ' ', column_type) 'column_type' from information_schema.columns \n" +
-                                        "where table_schema = 'trialdirect' and table_name = '#TAB#' order by ordinal_position";
-            auditTabColumnsSQL = auditTabColumnsSQL.replace("#TAB#", auditTableName);
-            List<TrialDirectTableDef> auditColumnsDef = jdbc.query(auditTabColumnsSQL, new TrialDirectColDefMapper());
-
-            String tabColumnsSQL = "select column_name, concat(column_name, ' ', column_type) from information_schema.columns\n" +
+            String tabColumnsSQL = "select column_name from information_schema.columns \n" +
                                    "where table_schema = 'trialdirect' and table_name = '#TAB#' order by ordinal_position";
             tabColumnsSQL = tabColumnsSQL.replace("#TAB#", table);
-            List<TrialDirectTableDef> actualColumnsDef = jdbc.query(tabColumnsSQL, new TrialDirectColDefMapper());
-
-            actualColumnsDef.retainAll(auditColumnsDef);
-
-
-
+            List<String> columnNames = jdbc.queryForList(tabColumnsSQL, String.class);
 
             StringBuilder asb = new StringBuilder();
             asb.append("create table ").append(auditTableName).append("(");
