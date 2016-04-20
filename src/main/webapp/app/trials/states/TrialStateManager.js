06f140da90849bc3ecff06cc718c8e5e26f1f132
Stephen <info@allie4.ie>
2016-04-20 15:43:47 +0100

    Trial Edit Page Creation, plus Trial Info search update

--- src/main/webapp/app/trials/states/TrialStateManager.js
+++ src/main/webapp/app/trials/states/TrialStateManager.js
@@ -35,6 +35,68 @@ angular.module('uiRouterSample.trial', [
                         url: '',
                         templateUrl:'app/trials/views/trials.create.html'
                     })
+                    /*.state('trials.edit', {
+                        url: '/edit/{trialId:[0-9]{1,4}}',
+                        templateUrl:'app/trials/views/trials.edit.html'
+                    })*/
+
+                    .state('trials.edit', {
+                        url: '/edit/{trialId:[0-9]{1,4}}',
+                        views: {
+
+                            // So this one is targeting the unnamed view within the parent state's template.
+                            '': {
+                                templateUrl: 'app/trials/views/trials.edit.html',
+                                controller: 'TrialEditController',
+                                resolve: {
+
+                                    trialInfo: function ($stateParams, TrialResourceService) {
+                                        return TrialResourceService.loadTrialInfo($stateParams.trialId);
+                                    },
+
+                                    trial: function ($stateParams, TrialResourceService) {
+                                        return TrialResourceService.loadTrial($stateParams.trialId);
+                                    },
+
+                                    questionnaireEntries: function ($stateParams, QuestionnaireEntryResourceService, Question, Answer) {
+                                        //Initialise the answer api
+                                        Question.initialize();
+                                        Answer.initialize();
+                                        QuestionnaireEntryResourceService.initialize();
+
+                                        // Load all the questions for the particular Trial
+                                        return QuestionnaireEntryResourceService.loadQuestionnaireEntriesForTherapeuticLink('trials', $stateParams.trialId);
+                                    }
+                                    ,
+                                    trialSelectorQuestionnaireEntries: function ($stateParams, TrialSelectorQuestionnaireEntryResourceService, Question, Answer) {
+                                        //Initialise the answer api
+                                        Question.initialize();
+                                        Answer.initialize();
+                                        TrialSelectorQuestionnaireEntryResourceService.initialize();
+
+                                        // Load all the questions for the particular Trial
+                                        return TrialSelectorQuestionnaireEntryResourceService.loadTrialSelectorQuestionnaireEntriesForTrial($stateParams.trialId);
+                                    }
+                                }
+                            },
+                            //'hint@': {
+                            //  template: 'This is trials.detail populating the "hint" ui-view'
+                            //},
+                            // This one is targeting the ui-view="menuTip" within the parent state's template.
+                            'menuTip': {
+                                // templateProvider is the final method for supplying a template.
+                                // There is: template, templateUrl, and templateProvider.
+                                templateProvider: ['$stateParams',
+                                    function ($stateParams) {
+                                        // This is just to demonstrate that $stateParams injection works for templateProvider.
+                                        // $stateParams are the parameters for the new state we're transitioning to, even
+                                        // though the global '$stateParams' has not been updated yet.
+                                        return '<hr><small class="muted">Trial ID: ' + $stateParams.trialId + '</small>';
+                                    }]
+                            }
+                        }
+                    })
+
                     .state('trials.detail', {
                         url: '/{trialId:[0-9]{1,4}}',
                         views: {
