be5ae14f2dc722871bba54f97db37e2f22a04b58
Mark Gargan <mark.gargan@gmail.com>
2016-04-28 10:23:04 +0100

    Loads the trialSites in the sql file after the tables and triggers have been initialized.

--- src/main/java/com/tekenable/config/prime/TrialDirectPrimer.java
+++ src/main/java/com/tekenable/config/prime/TrialDirectPrimer.java
@@ -1,15 +1,18 @@
 package com.tekenable.config.prime;
 
 import com.tekenable.config.AuditConfigurator;
-import com.tekenable.config.prime.trial.cancer.TrialPrimer;
 import com.tekenable.config.prime.trial.cancer.glaxo.GlaxoCancerPrimer;
 import com.tekenable.config.prime.trial.cancer.lundbeck.LundbeckCancerPrimer;
 import com.tekenable.config.prime.trial.cancer.pfizer.PfizerCancerPrimer;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.annotation.PostConstruct;
-import javax.transaction.Transactional;
+import org.springframework.core.io.Resource;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class TrialDirectPrimer {
 
@@ -29,13 +32,18 @@ public class TrialDirectPrimer {
     @Autowired
     private AuditConfigurator auditConfigurator;
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Value("classpath:primer/imports/trialSites.sql")
+    private Resource trialSitesResource;
 
     @PostConstruct
     public void initDB() throws IOException {
 
         this.configureTriggers();
         this.runPrimers();
-
+        this.runImportFiles();
     }
 
     private void configureTriggers() {
@@ -45,9 +53,12 @@ public class TrialDirectPrimer {
     private void runPrimers() throws IOException {
 
         cancerQuestionnairePrimer.initDB();
-//            glaxoPrimer.initDB();
         lundbeckPrimer.initDB();
         pfizerPrimer.initDB();
 
+    }
+
+    private void runImportFiles() throws IOException {
+        jdbcTemplate.batchUpdate(new String[]{IOUtils.toString(trialSitesResource.getInputStream())});
     }
 }
